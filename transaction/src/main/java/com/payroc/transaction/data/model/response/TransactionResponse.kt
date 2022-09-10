3b39a0b9f7177b8ca6f0a946b10cc2a27580c666
Oliver Zimmerman <oezimmerman@gmail.com>
2022-09-10 12:17:04 +0100

    chore: bump compile and target SDK versions

--- transaction/src/main/java/com/payroc/transaction/data/model/response/TransactionResponse.kt
+++ transaction/src/main/java/com/payroc/transaction/data/model/response/TransactionResponse.kt
@@ -5,48 +5,48 @@ import com.payroc.transaction.data.model.Order
 import com.payroc.transaction.data.model.request.CustomerAccount
 
 data class TransactionResponse(
-    @SerializedName("uniqueReference") var uniqueReference: String? = null,
-    @SerializedName("terminal") var terminal: String? = null,
-    @SerializedName("order") var order: Order? = Order(),
-    @SerializedName("customerAccount") var customerAccount: CustomerAccount? = CustomerAccount(),
-    @SerializedName("securityCheck") var securityCheck: SecurityCheck? = SecurityCheck(),
-    @SerializedName("transactionResult") var transactionResult: TransactionResult? = TransactionResult(),
+    @SerializedName("uniqueReference") var uniqueReference: String,
+    @SerializedName("terminal") var terminal: String,
+    @SerializedName("order") var order: Order,
+    @SerializedName("customerAccount") var customerAccount: CustomerAccount,
+    @SerializedName("securityCheck") var securityCheck: SecurityCheck,
+    @SerializedName("transactionResult") var transactionResult: TransactionResult,
     @SerializedName("additionalDataFields") var additionalDataFields: ArrayList<AdditionalDataFields> = arrayListOf(),
     @SerializedName("emvTags") var emvTags: ArrayList<EmvTags> = arrayListOf(),
 )
 
 data class SecurityCheck(
-    @SerializedName("cvvResult") var cvvResult: String? = null,
-    @SerializedName("avsResult") var avsResult: String? = null,
+    @SerializedName("cvvResult") var cvvResult: String,
+    @SerializedName("avsResult") var avsResult: String,
 )
 
 data class StoredPaymentCredentials(
-    @SerializedName("terminal") var terminal: String? = null,
-    @SerializedName("merchantReference") var merchantReference: String? = null,
-    @SerializedName("cardholderName") var cardholderName: String? = null,
-    @SerializedName("credentialsNumber") var credentialsNumber: String? = null,
-    @SerializedName("maskedPan") var maskedPan: String? = null,
-    @SerializedName("securityCheck") var securityCheck: String? = null,
+    @SerializedName("terminal") var terminal: String,
+    @SerializedName("merchantReference") var merchantReference: String,
+    @SerializedName("cardholderName") var cardholderName: String,
+    @SerializedName("credentialsNumber") var credentialsNumber: String,
+    @SerializedName("maskedPan") var maskedPan: String,
+    @SerializedName("securityCheck") var securityCheck: String,
 )
 
 data class TransactionResult(
-    @SerializedName("type") var type: String? = null,
-    @SerializedName("status") var status: String? = null,
-    @SerializedName("approvalCode") var approvalCode: String? = null,
-    @SerializedName("dateTime") var dateTime: String? = null,
-    @SerializedName("currency") var currency: String? = null,
-    @SerializedName("authorizedAmount") var authorizedAmount: Int? = null,
-    @SerializedName("resultCode") var resultCode: String? = null,
-    @SerializedName("resultMessage") var resultMessage: String? = null,
-    @SerializedName("storedPaymentCredentials") var storedPaymentCredentials: StoredPaymentCredentials? = StoredPaymentCredentials(),
+    @SerializedName("type") var type: String,
+    @SerializedName("status") var status: String,
+    @SerializedName("approvalCode") var approvalCode: String,
+    @SerializedName("dateTime") var dateTime: String,
+    @SerializedName("currency") var currency: String,
+    @SerializedName("authorizedAmount") var authorizedAmount: Int,
+    @SerializedName("resultCode") var resultCode: String,
+    @SerializedName("resultMessage") var resultMessage: String,
+    @SerializedName("storedPaymentCredentials") var storedPaymentCredentials: StoredPaymentCredentials,
 )
 
 data class AdditionalDataFields(
-    @SerializedName("name") var name: String? = null,
-    @SerializedName("value") var value: String? = null,
+    @SerializedName("name") var name: String,
+    @SerializedName("value") var value: String,
 )
 
 data class EmvTags(
-    @SerializedName("hex") var hex: String? = null,
-    @SerializedName("value") var value: String? = null,
+    @SerializedName("hex") var hex: String,
+    @SerializedName("value") var value: String,
 )
