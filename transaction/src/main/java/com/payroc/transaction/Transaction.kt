0c8c006204eb0086077e7866f0a9c4603ebc2f4c
Oliver Zimmerman <oezimmerman@gmail.com>
2022-09-15 11:44:18 +0100

    chore: format class

--- transaction/src/main/java/com/payroc/transaction/Transaction.kt
+++ transaction/src/main/java/com/payroc/transaction/Transaction.kt
@@ -28,7 +28,7 @@ class Transaction(
 
 
     private suspend fun authenticate(apiKey: String): String? {
-        var token: String? =  null
+        var token: String? = null
         val authResponse = _repository.authenticate(apiKey)
         authResponse.onSuccess {
             token = this.data.token
@@ -55,8 +55,8 @@ class Transaction(
     private suspend fun transactionRequest(token: String, card: Card) {
         var transactionRequest: TransactionRequest? = null
         when (card.payloadType) {
-             "EMV" -> {
-                 transactionRequest = TransactionRequest(
+            "EMV" -> {
+                transactionRequest = TransactionRequest(
                     terminal = terminal,
                     order = Order(orderId = createOrderID(),
                         totalAmount = amount,
@@ -66,13 +66,14 @@ class Transaction(
                         card.payloadType)
                 )
             }
-             "MAG_STRIPE" -> {
-                 transactionRequest = TransactionRequest(
+            "MAG_STRIPE" -> {
+                transactionRequest = TransactionRequest(
                     terminal = terminal,
                     order = Order(orderId = createOrderID(),
                         totalAmount = amount,
                         orderBreakdown = OrderBreakdown(subtotalAmount = amount)),
-                    customerAccount = CustomerAccount.MAGCustomerAccount(payloadType = card.payloadType, cardholderName = card.cardholdername!!,
+                    customerAccount = CustomerAccount.MAGCustomerAccount(payloadType = card.payloadType,
+                        cardholderName = card.cardholdername!!,
                         cardDetails = CardDetails(
                             device = Device(dataKsn = card.dataKsn,
                                 serialNumber = card.serialNumber),
@@ -84,60 +85,59 @@ class Transaction(
         }
         transactionRequest?.let {
             transactionListener.updateState(TransactionState.PROCESSING)
-            val transactionResponse = _repository.createTransaction(token, transactionRequest).onSuccess {
-                //ToDo handle success filter object and obtain receipts only
-                // transactionListener.receiptReceived(receipt)
-            }.onError {
-                //ToDo log state properly
-                Log.e("Error", "Error....")
+            val transactionResponse =
+                _repository.createTransaction(token, transactionRequest).onSuccess {
+                    //ToDo handle success filter object and obtain receipts only
+                    // transactionListener.receiptReceived(receipt)
+                }.onError {
+                    //ToDo log state properly
+                    Log.e("Error", "Error....")
 
-            }.onException {
-                //ToDo log state properly
-                Log.e("Error", "Error....")
-            }
+                }.onException {
+                    //ToDo log state properly
+                    Log.e("Error", "Error....")
+                }
         } ?: run {
             //ToDO update error state with message to client
             Log.e("Error", "Unknown card type used")
-        }}
+        }
+    }
+}
+
+private fun hexToBinary(hex: String): String {
+    val len = hex.length * 4
+    var bin: String = BigInteger(hex, 16).toString(2)
+
+    //left pad the string result with 0s if converting to BigInteger removes them.
+    if (bin.length < len) {
+        val diff = len - bin.length
+        var pad = ""
+        for (i in 0 until diff) {
+            pad += "0"
+        }
+        bin = pad + bin
+    }
+    return bin
+}
+
+// Move to utility?
+private fun generateTlv(card: Card): String {
+
+    val stringBuilder = StringBuilder()
+    // Step 1, convert Hex value to binary string
+    // Step 2, get the length of the binary string
+    // Step 3, divide binary string by 8 which is the bytes
+    card.tags?.forEach { tag ->
+        val bin = hexToBinary(tag.value)
+        val hex = Integer.toHexString(bin.length / 8)
+        stringBuilder.append(tag.key.uppercase())
+        if (hex.length < 2) {
+            stringBuilder.append("0" + hex.uppercase())
+        } else {
+            stringBuilder.append(hex.uppercase())
+        }
+        stringBuilder.append(tag.value.uppercase())
     }
 
-    private fun hexToBinary(hex: String): String {
-        val len = hex.length * 4
-        var bin: String = BigInteger(hex, 16).toString(2)
-
-        //left pad the string result with 0s if converting to BigInteger removes them.
-        if (bin.length < len) {
-            val diff = len - bin.length
-            var pad = ""
-            for (i in 0 until diff) {
-                pad += "0"
-            }
-            bin = pad + bin
-        }
-        return bin
-    }
-
-    // Move to utility?
-     private fun generateTlv(card: Card): String {
-
-        val stringBuilder = StringBuilder()
-        // Step 1, convert Hex value to binary string
-        // Step 2, get the length of the binary string
-        // Step 3, divide binary string by 8 which is the bytes
-        card.tags?.forEach { tag ->
-            val bin = hexToBinary(tag.value)
-            val hex = Integer.toHexString(bin.length/8)
-            stringBuilder.append(tag.key.uppercase())
-            if (hex.length < 2) {
-                stringBuilder.append("0"+hex.uppercase())
-            } else {
-                stringBuilder.append(hex.uppercase())
-            }
-            stringBuilder.append(tag.value.uppercase())
-        }
-
-        return stringBuilder.toString()
-     }
-
-
+    return stringBuilder.toString()
 }
