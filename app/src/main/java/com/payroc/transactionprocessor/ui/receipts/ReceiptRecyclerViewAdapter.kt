cfaf5c20c61ffa62f351a4d11515a54dc024e153
Oliver Zimmerman <oezimmerman@gmail.com>
2022-09-16 23:50:54 +0100

    feat: format receipt

--- app/src/main/java/com/payroc/transactionprocessor/ui/receipts/ReceiptRecyclerViewAdapter.kt
+++ app/src/main/java/com/payroc/transactionprocessor/ui/receipts/ReceiptRecyclerViewAdapter.kt
@@ -1,13 +1,22 @@
 package com.payroc.transactionprocessor.ui.receipts
 
+import android.content.Context
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.payroc.transaction.data.model.response.Receipts
 import com.payroc.transactionprocessor.database.entities.Receipt
 import com.payroc.transactionprocessor.databinding.ReceiptViewBinding
 
+
 class ReceiptRecyclerViewAdapter(val receipts: List<Receipt>) :
     RecyclerView.Adapter<ReceiptRecyclerViewAdapter.ReceiptViewHolder>() {
+
+    // Initialize context with overridden onAttachedToRecyclerView
+    private lateinit var context: Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReceiptViewHolder {
         val binding = ReceiptViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -15,8 +24,44 @@ class ReceiptRecyclerViewAdapter(val receipts: List<Receipt>) :
     }
 
     override fun onBindViewHolder(holder: ReceiptViewHolder, position: Int) {
-        holder.binding.idTextView.text = receipts[position].uniqueReference.toString()
-        holder.binding.receiptTextView.text = receipts[position].receipts
+
+        //ToDo loop through merchantDetails and transactionData, create new textView and add view to linear layout
+
+        val receipt = receipts[position].receipts
+        val gson = Gson()
+        val receiptData = gson.fromJson(receipt, Receipts::class.java)
+
+        receiptData.merchantDetails.forEach { merchantDetails ->
+
+            // Handle label - bold, etc
+            val labelText = TextView(context)
+            labelText.text = merchantDetails.label
+            labelText.setTypeface(null, Typeface.BOLD)
+            holder.binding.receiptLinearLayout.addView(labelText)
+
+            // Handle value - standard
+            val valueText = TextView(context)
+            valueText.text = merchantDetails.value
+            holder.binding.receiptLinearLayout.addView(valueText)
+        }
+        receiptData.transactionData.forEach { transactionData ->
+            // Handle label - bold, etc
+            val labelText = TextView(context)
+            labelText.text = transactionData.label
+            labelText.setTypeface(null, Typeface.BOLD)
+            holder.binding.receiptLinearLayout.addView(labelText)
+
+            // Handle value - standard
+            val valueText = TextView(context)
+            valueText.text = transactionData.value
+            holder.binding.receiptLinearLayout.addView(valueText)
+        }
+    }
+
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        context = recyclerView.context
     }
 
     override fun getItemCount(): Int {
@@ -26,3 +71,4 @@ class ReceiptRecyclerViewAdapter(val receipts: List<Receipt>) :
     inner class ReceiptViewHolder(val binding: ReceiptViewBinding)
         :RecyclerView.ViewHolder(binding.root)
 }
+
