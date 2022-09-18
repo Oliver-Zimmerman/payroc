29e4986b70f8714ccaaaa7cc0e69e667309573ec
Oliver Zimmerman <oezimmerman@gmail.com>
2022-09-18 10:33:51 +0100

    chore: comment the Utilities class

--- transaction/src/main/java/com/payroc/transaction/utility/Utilities.kt
+++ transaction/src/main/java/com/payroc/transaction/utility/Utilities.kt
@@ -4,6 +4,9 @@ import com.payroc.transaction.data.model.Card
 import java.lang.StringBuilder
 import java.math.BigInteger
 
+/**
+ * Utility helper method to generate a 6 character Order ID containing A-Z (uppercase) and 0-9
+ */
 internal fun createOrderID(): String {
     val allowedChars = ('A'..'Z') + ('0'..'9')
     return (1..6)
@@ -11,6 +14,9 @@ internal fun createOrderID(): String {
         .joinToString("")
 }
 
+/**
+ * Utility helper method to convert a hex String to binary String
+ */
 internal fun hexToBinary(hex: String): String {
     val len = hex.length * 4
     var bin: String = BigInteger(hex, 16).toString(2)
@@ -27,7 +33,19 @@ internal fun hexToBinary(hex: String): String {
     return bin
 }
 
-// Move to utility?
+/**
+ * Utility helper method to create a TLV String with a provided [Card]
+ *
+ * A TLV String is generate with the following steps:
+ * 1. Convert Hex String ([Card.tags] value field) to Binary String
+ * 2. Get the length of the converted Binary String
+ * 3. Divide the calculated length by 8, which is the number of bytes
+ * 4. Use the number of bytes and create an [Integer.toHexString]
+ * 5. Append the tag, the new hex string, and value together. Adding a 0 to the start of the hex
+ *    value if its length is smaller than 2
+ * 6. Loop through all tags and append them all together with this process.
+ *
+ */
 internal fun generateTlv(card: Card): String {
 
     val stringBuilder = StringBuilder()
